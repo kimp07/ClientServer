9ea20197470c52aff3e1dd010b87b845231a2270
kimp07 <kimp07@gmail.com>
2021-01-02 20:52:06 +0200

    Studying java

---
+++ Phone/src/org/alex/phone/Phone.java
@@ -0,0 +1,65 @@
+package org.alex.phone;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Phone implements Closeable {
+
+    private final Socket socket;
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+
+    public Phone(String ipAddress, int ipPort) {
+        try {
+            this.socket = new Socket(ipAddress, ipPort);
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Phone(ServerSocket server) {
+        try {
+            this.socket = server.accept();
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeLine(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private BufferedReader createReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    private BufferedWriter createWriter() throws IOException {
+        return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void close() throws IOException {
+        reader.close();
+        writer.close();
+        socket.close();
+    }
+}
